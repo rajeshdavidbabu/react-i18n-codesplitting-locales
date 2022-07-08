4f7c1a4e85ae5e08a3dec08d74c710bc09b250b5
Rajesh Babu <>
2022-07-08 17:01:15 +0200

    Switch language and dynamic loading

--- src/Component.tsx
+++ src/Component.tsx
@@ -5,18 +5,29 @@ const Component = () => {
   const { t, i18n } = useTranslation();
 
   // Change language without re-loading application.
-  const loadJapanese = async () => {
-    const resource = await import('./locales/ja.json');
+  const switchLanguage = async (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const langKey = event.target.value;
 
-    await i18n.addResourceBundle('ja', 'translation', resource.default.translation);
-    await i18n.changeLanguage('ja');
+    try {
+      const resource = await import(`./locales/${langKey}.json`);
+
+      await i18n.addResourceBundle(langKey, 'translation', resource.default.translation);
+      await i18n.changeLanguage(langKey);
+    } catch (error) {
+      console.log('The langKey is possibly invalid ', error);
+    }
   };
 
   return (
     <div>
       <h2>Minimal React TS App with React-i18next Lazyloading</h2>
       <p>{t('welcome')}</p>
-      <button onClick={loadJapanese}>Change to Japanese</button>
+      <select onChange={switchLanguage}>
+        <option>Select Language</option>
+        <option value="en">English</option>
+        <option value="ja">Japan</option>
+        <option value="fr">French</option>
+      </select>
     </div>
   );
 };
